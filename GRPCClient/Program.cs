a84f7e5e64695f7eb878a81430959de7e2b02149
Mervin D'Souza <mdsouza@york.local>
2019-12-27 13:36:17 -0600

    Customer Service added and running.

--- GRPCClient/Program.cs
+++ GRPCClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using GrpcServer;
 using GRPCServer;
 
 namespace GRPCClient
@@ -17,12 +18,17 @@ namespace GRPCClient
             httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
             var httpClient = new HttpClient(httpClientHandler);
 
-            var input = new HelloRequest { Name = "World!" };
-            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(input);
+            var greeterInput = new HelloRequest { Name = "World!" };
+            var greeterChannel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+            var greeterclient = new Greeter.GreeterClient(greeterChannel);
+            var greeterReply = await greeterclient.SayHelloAsync(greeterInput);
 
-            Console.WriteLine(reply.Message);
+            var customerInput = new CustomerLookupModel { UserId = 1 };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            var customerReply = await customerClient.GetCustomerInfoAsync(customerInput);
+
+            Console.WriteLine(customerReply.FirstName);
             Console.ReadLine();
         }
     }
---
+++ GRPCServer/Services/CustomersService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using GrpcServer;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GRPCServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+            if (request.UserId == 1)
+            {
+                output.FirstName = "User 1";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "User 2";
+            }
+            else
+            {
+                output.FirstName = "All Other Users";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
