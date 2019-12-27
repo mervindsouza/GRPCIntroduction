72869ac89c288c716d415bed24f8e2a840bf6604
mdtaf <mdsouza@transactfutures.com>
2019-12-27 11:29:09 -0600

    Ignored Dev Certs. Hello World Running.

--- GRPCClient/Program.cs
+++ GRPCClient/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GRPCServer;
 
 namespace GRPCClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ignore invalid development certificate for linux distros
+            // use dotnet dev-certs https --trust on windows or mac os
+
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+
+            var input = new HelloRequest { Name = "World!" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
         }
     }
 }
